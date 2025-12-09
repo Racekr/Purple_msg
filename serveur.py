ed34a8e208fc6c72d0984b4b49163551a5dc0e59
Purple_key <purple.key82.82.82@gmail.com>
2025-12-09 18:35:26 +0100

    amdin password o secure

--- serveur.py
+++ serveur.py
@@ -7,6 +7,7 @@ SERVER_PASSWORD = os.getenv("SERVER_PASSWORD")
 MONGO_URI = os.getenv("MONGO_URI")  # À configurer sur Render
 
 admin_username = "Purple_key"
+admin_password = "1234"
 
 # MongoDB setup
 mongo_client = AsyncIOMotorClient(MONGO_URI)
@@ -105,7 +106,7 @@ async def ws_handler(request):
             _, user, upass = msg.split(" ", 2)
 
             user_data = await get_user(user)
-            is_valid = (user == admin_username) or (user_data and user_data["password"] == upass)
+            is_valid = (user == admin_username and user_sata["password" == admin_password]) or (user_data and user_data["password"] == upass)
 
             if is_valid:
                 clients[ws] = user
